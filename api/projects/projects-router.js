a0eb1b7c07c31a7398d9a1b0ef47be0e4331ca46
bahasaracoglu <bahasaracoglu@gmail.com>
2023-05-19 21:21:12 +0300

    ilk kurulum, projects-router

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,25 @@
 // "project" routerını buraya yazın!
+const express = require("express");
+const projectsModel = require("./projects-model");
+const router = express.Router();
+
+router.get("/", async (req, res, next) => {
+  try {
+    const allPosts = await projectsModel.get();
+    if (!allPosts) {
+      res.json([]);
+    } else {
+      res.json(allPosts);
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/", async (req, res, next) => {});
+router.post("/", async (req, res, next) => {});
+router.put("/", async (req, res, next) => {});
+router.delete("/", async (req, res, next) => {});
+router.get("/", async (req, res, next) => {});
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,6 +1,10 @@
-const express = require('express');
+const express = require("express");
 const server = express();
+const projectsRouter = require("./projects/projects-router");
 
+server.use(express.json());
+
+server.use("/api/projects", projectsRouter);
 // Sunucunuzu yapılandırın
 // Eylem routerınızı /api/actions/actions-router.js içinde oluşturun
 // Proje roterlarınızı /api/projects/projects-router.js içinde oluşturun
