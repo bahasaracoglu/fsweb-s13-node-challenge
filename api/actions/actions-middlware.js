e6b32ef91d32ac72bd84113388f474a81a627a2c
bahasaracoglu <bahasaracoglu@gmail.com>
2023-05-20 00:10:20 +0300

    actions post & validate

--- api/actions/actions-middlware.js
+++ api/actions/actions-middlware.js
@@ -1,12 +1,13 @@
 // eylemlerle ilgili ara katman yazılımları yazın
 
 const actionsModel = require("./actions-model");
+const projectModel = require("../projects/projects-model");
 
 async function validateActionId(req, res, next) {
   try {
     const action = await actionsModel.get(req.params.id);
     if (!action) {
-      res.status(404).json({ message: "Action not found" });
+      res.status(404).json({ message: "Action is not found" });
     } else {
       req.currentAction = action;
       next();
@@ -16,4 +17,22 @@ async function validateActionId(req, res, next) {
   }
 }
 
-module.exports = { validateActionId };
+async function validatePost(req, res, next) {
+  const { description, project_id, notes } = req.body;
+  try {
+    if (description.length > 128 || !description || project_id <= 0 || !notes) {
+      res.status(400).json({ message: "Please check the fields" });
+    } else {
+      const projectExists = await projectModel.get(project_id);
+      if (!projectExists) {
+        res
+          .status(404)
+          .json({ message: "Project with the given id does not exist" });
+      } else {
+        next();
+      }
+    }
+  } catch (error) {}
+}
+
+module.exports = { validateActionId, validatePost };
--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -15,13 +15,30 @@ router.get("/", async (req, res, next) => {
 
 router.get("/:id", actionsMw.validateActionId, (req, res, next) => {
   try {
-    res.json(req.currentAciton);
+    res.json(req.currentAction);
   } catch (error) {
     next(error);
   }
 });
 
-router.post("/", async (req, res, next) => {});
+router.post("/", actionsMw.validatePost, async (req, res, next) => {
+  const { project_id, notes, description, completed } = req.body;
+
+  let postData = {
+    project_id: project_id,
+    notes: notes,
+    description: description,
+    completed: completed,
+  };
+
+  try {
+    const insertedPost = await actionsModel.insert(postData);
+    res.json(insertedPost);
+  } catch (error) {
+    next(error);
+  }
+});
+
 router.put("/", async (req, res, next) => {});
 router.delete("/", async (req, res, next) => {});
 
