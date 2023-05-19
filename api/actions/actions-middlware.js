2d3f1c482136144a27021d3882c16ad233f99c8d
bahasaracoglu <bahasaracoglu@gmail.com>
2023-05-19 23:02:40 +0300

    actions router

--- api/actions/actions-middlware.js
+++ api/actions/actions-middlware.js
@@ -1,1 +1,19 @@
 // eylemlerle ilgili ara katman yazılımları yazın
+
+const actionsModel = require("./actions-model");
+
+async function validateActionId(req, res, next) {
+  try {
+    const action = await actionsModel.get(req.params.id);
+    if (!action) {
+      res.status(404).json({ message: "Action not found" });
+    } else {
+      req.currentAction = action;
+      next();
+    }
+  } catch (error) {
+    next(error);
+  }
+}
+
+module.exports = { validateActionId };
--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,1 +1,28 @@
 // "eylem" routerını buraya yazın
+const express = require("express");
+const actionsModel = require("./actions-model");
+const actionsMw = require("./actions-middlware");
+const router = express.Router();
+
+router.get("/", async (req, res, next) => {
+  try {
+    const allActions = await actionsModel.get();
+    res.json(allActions || []);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/:id", actionsMw.validateActionId, (req, res, next) => {
+  try {
+    res.json(req.currentAciton);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post("/", async (req, res, next) => {});
+router.put("/", async (req, res, next) => {});
+router.delete("/", async (req, res, next) => {});
+
+module.exports = router;
